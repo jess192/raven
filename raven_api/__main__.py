a78595e82af3d5f46eeb577ba73d65a359a3d9c1
Jess <79601842+jess192@users.noreply.github.com>
2022-08-01 16:17:06 -0400

    refactor(api): add support for ssl config in .env

--- raven_api/__main__.py
+++ raven_api/__main__.py
@@ -1,16 +1,21 @@
 from raven_core.logging.logger import configure_logging, logger
+import os
+from dotenv import load_dotenv
 import uvicorn
 
 
 if __name__ == '__main__':
     configure_logging()
     logger.info('Running Raven API')
+    load_dotenv()
 
     try:
         uvicorn.run(
             app='raven_api.server:app',
             host='0.0.0.0',
-            port=8090
+            port=int(os.getenv('RAVEN_API_PORT')),
+            ssl_keyfile=os.getenv('SSL_KEYFILE'),
+            ssl_certfile=os.getenv('SSL_CERT_FILE')
         )
     except KeyboardInterrupt:
         logger.info('KeyboardInterrupt: exiting raven_api')
